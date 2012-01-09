fa98e29e2d0d1cd3baaa24796bab6b3bf0e8b8b7
jgs <jgs@jgss-MacBook-Pro.local>
2012-01-09 18:04:46 -0500

    Initial Commit

---
+++ ohms/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ohms
+//
+//  Created by Jack Shultz on 1/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
